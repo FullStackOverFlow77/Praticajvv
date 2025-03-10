044a9dfe0890fb79c54bae71d62c5a284fcfd798
João Victor Mendes <NightFace777@gmail.com>
2025-03-10 18:22:46 +0000

    save44

--- joao.java
+++ joao.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 public class joao{
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
         int A,B,soma ; 
 
         A = sc.nextInt();
         B = sc.nextInt();
 
-        soma = A + B  
+        soma = A + B  ;
+        System.out.println("Soma= "+soma);
 
-        
+        if () {
+            
+        }
+
+            sc.close();
         
     }
 }
