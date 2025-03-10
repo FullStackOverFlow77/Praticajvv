e1515865d99cfa32864b46bff9ee157b5a746a15
João Victor Mendes <NightFace777@gmail.com>
2025-03-10 18:16:07 +0000

    luiz

---
+++ joao.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class joao{
+    public static void main(String[] args) {
+        
+        int A,B,soma ; 
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        soma = A + B  
+
+        
+        
+    }
+}
